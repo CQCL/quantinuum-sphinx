6c7472a247cdf7001762dde9390d1fbc772d6194
aidanCQ <aidan.keay@quantinuum.com>
2024-07-20 22:14:59 +0100

    Mobile cleanup.

--- ui/navmenu/MainNavigation.tsx
+++ ui/navmenu/MainNavigation.tsx
@@ -3,6 +3,7 @@ import React, { ComponentProps } from 'react'
 import { Link, navConfig } from './config'
 import { QuantinuumLogo } from './QuantinuumLogo'
 import { MobileMenu } from './MobileMenu'
+import { QuantinuumIdent } from './QuantinuumIdent'
 export const MainNavigation = (props: {
   activePath: string
   linkComponent?: Link
@@ -12,13 +13,17 @@ export const MainNavigation = (props: {
     : (props: ComponentProps<'a'>) => <a {...props}></a>
   return (
     <div className="bg-background text-foreground border-border sticky top-0 z-[100] w-full border-b text-sm">
-      <div className=" bg-background px-4 mx-auto max-w-[90rem] flex h-14 items-center justify-between">
+      <div className=" bg-background px-3 md:px-4 mx-auto max-w-[90rem] flex h-14 items-center justify-between">
         <div className="mr-4 flex items-center">
         <div className='block md:hidden mr-3'>
             <MobileMenu></MobileMenu>
             </div>
           <div className="whitespace-nowrap flex items-center gap-2">
-            <QuantinuumLogo></QuantinuumLogo>
+            <div className='hidden sm:block'><QuantinuumLogo></QuantinuumLogo>
+            </div>
+            <div className='block sm:hidden'>
+            <QuantinuumIdent/>
+            </div>
             <div className="text-muted-foreground text-xs font-medium flex items-center gap-1.5">
               <div>|</div><div>{navConfig.navProductName}</div>
             </div>
@@ -33,7 +38,7 @@ export const MainNavigation = (props: {
           <div className="flex items-center gap-2">
             {navConfig.navIconLinks.map(link => {
                 return <Link href={link.href} target='_blank' key={link.title}>
-               <img src={link.iconImageURL} className='w-6 h-6 hover:opacity-70 transition'></img>
+               <img src={link.iconImageURL} className='flex-shrink-0 min-w-6 max-w-6 min-h-6 max-h-6 hover:opacity-70 transition'></img>
              </Link>
             
             })}
