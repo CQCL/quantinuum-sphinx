8a5e5727c90fc5d2c1bbf209ff58e16794f083bb
aidanCQ <aidan.keay@quantinuum.com>
2024-09-17 14:52:24 +0100

    Cleanup.

--- react/tsup.config.ts
+++ react/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig([
   // Script for injecting nav into sphinx build.
   {
-  entry: ['./src/scripts/nav'],
+  entry: ['./src/injectNav.tsx'],
   outDir: 'build',
   minify: true,
   skipNodeModulesBundle: false,
@@ -14,7 +14,7 @@ export default defineConfig([
 }, 
 // Script for syncing dark mode preference
 {
-  entry: ['./syncTheme.ts'],
+  entry: ['./src/syncTheme.ts'],
   outDir: 'build',
   minify: true,
   skipNodeModulesBundle: false,
