31ae24b626b9f16f79a1d31c00599b108adb8e0f
aidanCQ <aidan.keay@quantinuum.com>
2024-09-17 15:12:01 +0100

    Fix workflow.

--- build-dist.sh
+++ build-dist.sh
@@ -3,7 +3,7 @@ mkdir ./dist
 cp -a quantinuum_sphinx/. dist/
 echo "🔨 Generating UI assets..."
 cd ./react
-npm i
+npm ci
 npm run build
 cp ./build/injectNav.global.js ../dist/_static/injectNav.global.js
 cp ./build/syncTheme.global.js ../dist/_static/syncTheme.global.js
