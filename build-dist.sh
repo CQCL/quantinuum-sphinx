3697a776106e3e7c1b135ac07981a9e3b7e3ac0d
aidanCQ <aidan.keay@quantinuum.com>
2024-09-17 15:05:04 +0100

    Fix copy.

--- build-dist.sh
+++ build-dist.sh
@@ -5,7 +5,7 @@ echo "🔨 Generating UI assets..."
 cd ./react
 npm i
 npm run build
-cp ./build/index.global.js ../dist/_static/index.global.js
+cp ./build/injectNav.global.js ../dist/_static/injectNav.global.js
 cp ./build/syncTheme.global.js ../dist/_static/syncTheme.global.js
 cp ./node_modules/@cqcl/quantinuum-ui/dist/tokens.css ../dist/_static/tokens.css
 npx tailwindcss --postcss ./postcss.config.cjs -i ./index.css -o ../dist/_static/tailwind.css
