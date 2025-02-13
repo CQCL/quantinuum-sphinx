f46f85c8343be0aa317d6419348974d4782a4ff3
Irfan Khan <irfan.khan@quantinuum.com>
2025-02-13 14:52:37 +0000

    update to @quantinuum/quantinuum-ui in build.sh

--- build-dist.sh
+++ build-dist.sh
@@ -7,7 +7,7 @@ npm ci
 npm run build
 cp ./build/injectNav.global.js ../dist/_static/injectNav.global.js
 cp ./build/syncTheme.global.js ../dist/_static/syncTheme.global.js
-cp ./node_modules/@cqcl/quantinuum-ui/dist/tokens.css ../dist/_static/tokens.css
+cp ./node_modules/@quantinuum/quantinuum-ui/dist/tokens.css ../dist/_static/tokens.css
 npx tailwindcss --postcss ./postcss.config.cjs -i ./index.css -o ../dist/_static/tailwind.css
 echo ✅ "Done. Added UI assets to dist."
 cd ../
